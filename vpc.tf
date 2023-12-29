e54669decdd6ce27c5fec5baf15716b90139d903
orjujeng <orjujeng@hotmail.com>
2023-12-29 23:35:52 +0800

    addsg

--- vpc.tf
+++ vpc.tf
@@ -115,3 +115,160 @@ resource "aws_route_table_association" "inside_1c_outside" {
 }
 
 #security group
+#ec2
+resource "aws_security_group" "orjujeng_ec2_sg" {
+  name        = "orjujeng_ec2_sg"
+  description = "Allow SSH Http Https traffic"
+  vpc_id      = aws_vpc.orjujeng_vpc.id
+  ingress {
+    description = "SSH connect EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http to EC2"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https to EC2"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "orjujeng_ec2_sg"
+  }
+}
+
+#lb
+resource "aws_security_group" "orjujeng_lb_sg" {
+  name        = "orjujeng_lb_sg"
+  description = "Allow Http Https traffic"
+  vpc_id      = aws_vpc.orjujeng_vpc.id
+
+  ingress {
+    description = "http to lb"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https to lb"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "orjujeng_lb_sg"
+  }
+}
+
+#rds
+resource "aws_security_group" "orjujeng_rds_sg" {
+  name        = "orjujeng_rds_sg"
+  description = "Allow ec2 to rds"
+  vpc_id      = aws_vpc.orjujeng_vpc.id
+
+  ingress {
+    description = "ec2 to rds"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.orjujeng_ec2_sg.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "orjujeng_rds_sg"
+  }
+}
+
+#redis
+resource "aws_security_group" "orjujeng_redis_sg" {
+  name        = "orjujeng_redis_sg"
+  description = "Allow ec2 to redis"
+  vpc_id      = aws_vpc.orjujeng_vpc.id
+
+  ingress {
+    description = "ec2 to redis"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    security_groups = [aws_security_group.orjujeng_ec2_sg.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "orjujeng_redis_sg"
+  }
+}
+
+#efs
+resource "aws_security_group" "orjujeng_efs_sg" {
+  name        = "orjujeng_efs_sg"
+  description = "Allow ec2 to efs"
+  vpc_id      = aws_vpc.orjujeng_vpc.id
+
+  ingress {
+    description = "ec2 to efs"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    security_groups = [aws_security_group.orjujeng_ec2_sg.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "orjujeng_efs_sg"
+  }
+}
